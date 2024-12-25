7296530dfd58d9ee2b1a2eb9245c92c7cf6702be
Pham-anh-Hub <phamphuonganhcutee@gmail.com>
2024-12-25 12:13:52 +0700

    Update Bai5_ss21.c

--- Bai5_ss21.c
+++ Bai5_ss21.c
@@ -1,18 +1,31 @@
 #include<stdio.h>
-
+#include<string.h> 
 int main(){
 	int n;
 	FILE *fptr = fopen("bt05.txt", "w");
 	printf("Moi nhap so dong: ");
 	scanf("%d", &n);
 	char sentence[50];
+	 
 	for(int i=0; i<n; i++){
 		fflush(stdin);
 		printf("\nNhap noi dung: ");
 		fgets(sentence, 50, stdin);
-		
 		fprintf(fptr, "%s", sentence);
 	}
+	fclose(fptr); 
+	
+	fptr = fopen("bt05.txt","r");
+	if(fptr==NULL){
+		printf("Loi mo file");
+		return 1; 
+	}
+	
+	for(int i=0; i<n; i++){
+		char readSentence[50];
+		fgets(readSentence, 50, fptr);
+		printf("\nDong %d: %s",i+1, readSentence);
+	} 
 	fclose(fptr);
 	return 0;
 }
